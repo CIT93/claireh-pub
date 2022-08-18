fe410e1c434a8c8cb08371ce9c7a177c5f1c8859
ClaireHalverson <cjahalverson@gmail.com>
2022-08-18 19:07:24 +0000

    Week 2 Learn Together

---
+++ w2/apps.js
@@ -0,0 +1,43 @@
+//Question to be answered: Which homework should I work on today
+//let Mon = 1 tues = 2 wed = 3 thurs = 4 fri = 5
+const showOnPage = function (text) {
+   let newParagraph = document.createElement("p")
+   newParagraph.innerHTML = text
+   let outputDiv = document.getElementById("output")
+   outputDiv.append(newParagraph)
+ }
+
+let currentday = 2
+let cit93workdue = true
+let cit28workdue = true
+let cit58workdue = false
+
+if(currentday === 2) {
+   showOnPage('Today is a CIT58 Class day from 6pm-10pm. No homework needs to be done')
+} else if (cit28workdue && cit58workdue) {
+   let cit93workdue = false
+    currentday !== 2
+    showOnPage('Work on SQL and Ethical Hacking after work')
+} else if (cit58workdue && cit93workdue) {
+   let cit28workdue = false
+    currentday !== 2
+    showOnPage('Work on Ethical Hacking and JavaScript after work')
+} else if (cit93workdue && cit28workdue) {
+    let cit58workdue = false
+    currentday !== 2
+    showOnPage('Work on JavaScript and SQL after work')
+} else if (cit28workdue){ 
+    let cit58workdue = false
+    let cit93workdue = false
+    showOnPage('Work on SQL after work')
+ } else if (cit58workdue){
+    let cit93workdue = false
+    let cit28workdue = false
+    showOnPage('Work on Ethical Hacking after work')
+ } else if (cit93workdue) {
+    let cit28workdue = false
+    let cit58workdue = false
+    showOnPage('work on Javascript after work')
+ }
+ else 
+   showOnPage('do whatever you want')
