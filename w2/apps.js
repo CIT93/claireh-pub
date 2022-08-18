70698c5f1a34c931d437365ccac1ebe401e86ef8
ClaireHalverson <cjahalverson@gmail.com>
2022-08-18 20:10:07 +0000

    Week 2 Learn Together

--- w2/apps.js
+++ w2/apps.js
@@ -8,16 +8,18 @@ const showOnPage = function (text) {
    outputDiv.append(newParagraph)
  }
 
- showOnPage('This output is based on the following global variable data')
- showOnPage('Current Day (Mon = 1 tues = 2 wed = 3 thurs = 4 fri = 5)----->' = currentday)
- showOnPage('Do I have anything due for JavaScript?----->' = cit93workdue)
- showOnPage('Do I have anything due for SQL?----->' = cit28workdue)
- showOnPage('Do I have anything due for Ethical Hacking?----->' = cit58workdue)
- 
+
 let currentday = 3
 let cit93workdue = true
-let cit28workdue = false
+let cit28workdue = true
 let cit58workdue = false
+
+showOnPage('This output is based on the following global variable data')
+showOnPage('Current Day (Mon = 1 Tues = 2 Wed = 3 Thurs = 4 Fri = 5)----->' + currentday)
+showOnPage('Do I have anything due for JavaScript?----->' + cit93workdue)
+showOnPage('Do I have anything due for SQL?----->' + cit28workdue)
+showOnPage('Do I have anything due for Ethical Hacking?----->' + cit58workdue)
+
 
 if(currentday === 2) {
    showOnPage('Today is a CIT58 Class day from 6pm-10pm. No homework needs to be done')
@@ -44,8 +46,8 @@ if(currentday === 2) {
  } else if (cit93workdue) {
     let cit28workdue = false
     let cit58workdue = false
-    showOnPage('work on Javascript after work')
+    showOnPage('Work on Javascript after work')
  }
  else {
-   showOnPage('do whatever you want')
+   showOnPage('Do whatever you want')
  }
