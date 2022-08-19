f1736c2f592c081aee98edeb0266e031db7bb160
ClaireHalverson <cjahalverson@gmail.com>
2022-08-19 00:26:34 +0000

    Week 2 Learn Together

--- w2/apps.js
+++ w2/apps.js
@@ -10,13 +10,13 @@ const showOnPage = function (text) {
 
 
 let currentday = 3
-let cit93workdue = false
-let cit28workdue = false
+let cit93workdue = true
+let cit28workdue = true
 let cit58workdue = false
-let cit93timerequirement = 2
+let cit93timerequirement = 3
 let cit28timerequirement = 3
 let cit58timerequirement = 2
-let timeavailable = 5
+let timeavailable = 15
 
 showOnPage('This output is based on the following global variable data')
 showOnPage('Current Day (Mon = 1 Tues = 2 Wed = 3 Thurs = 4 Fri = 5)----->' + currentday)
@@ -28,23 +28,28 @@ showOnPage('Do I have anything due for Ethical Hacking?----->' + cit58workdue)
 if(currentday === 2) {
    showOnPage('Today is a CIT58 Class day from 6pm-10pm. No homework needs to be done')
 } else if (cit28workdue && cit58workdue) {
-   let time = cit28timerequirement + cit58timerequirement - timeavailable
+   let time = timeavailable - (cit28timerequirement + cit58timerequirement)
     showOnPage('Work on SQL and Ethical Hacking after work')
+    showOnPage('Time Available After Homework' + time)
 } else if (cit58workdue && cit93workdue) {
-   let time = cit58timerequirement + cit93timerequirement - timeavailable
-    showOnPage('Work on Ethical Hacking and JavaScript after work')
+   let time = timeavailable - (cit58timerequirement + cit93timerequirement)
+    showOnPage('Time Available After Homework' + time)
 } else if (cit93workdue && cit28workdue) {
-  let time = cit93timerequirement + cit28timerequirement - timeavailable
+  let time = timeavailable - (cit93timerequirement + cit28timerequirement)
     showOnPage('Work on JavaScript and SQL after work')
+    showOnPage('Time Available After Homework' + time)
 } else if (cit28workdue){ 
-   let time = cit28timerequirement - timeavailable
+   let time = timeavailable - (cit28timerequirement)
     showOnPage('Work on SQL after work')
+    showOnPage('Time Available After Homework' + time)
  } else if (cit58workdue){
-   let time = cit58timerequirement - timeavailable
+   let time = timeavailable - (cit58timerequirement)
     showOnPage('Work on Ethical Hacking after work')
+    showOnPage('Time Available After Homework' + time)
  } else if (cit93workdue) {
-   let time = cit93timerequirement - timeavailable
+   let time = timeavailable - (cit58timerequirement)
     showOnPage('Work on Javascript after work')
+    showOnPage('Time Available After Homework' + time)
  }
  else {
    showOnPage('Do whatever you want')
