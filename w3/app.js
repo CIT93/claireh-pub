19379e4fe1fbbe7bcfea247fc09fdcbce17ec931
ClaireHalverson <cjahalverson@gmail.com>
2022-08-25 17:47:34 +0000

    Learn Together W3

--- w3/app.js
+++ w3/app.js
@@ -4,17 +4,26 @@ const showOnPage = function (text) {
     let outputDiv = document.getElementById("output")
     outputDiv.append(newParagraph)
   }
-  
+
+showOnPage('This output is based on the question which class am I working on, and how much time do I need to invest')
+showOnPage('Class today is CIT:' + classnum)
+showOnPage('Homework time is:' + homeworktime)
+showOnPage('Free time is:'+ freetime)
+showOnPage('if freetime is negative. Allocate additonal hours from Friday')
 
  let homework = function (classnum, homeworktime) {
 let freetime = (4 - homeworktime)
 if (classnum = 58) {
- 
-}if (classnum = 28){
 
-}if (classnum = 93)
+} else if (classnum = 28){
+
+}else if (classnum = 93){
+
+
+} 
 
 return `You have to do work for CIT ${classnum} and you have ${homeworktime} hour(s) of homework. This will leave you with ${freetime} hour(s) of freetime.`
  }
+ 
 let homeworkcalc = homework (28,5)
 showOnPage(homeworkcalc)
