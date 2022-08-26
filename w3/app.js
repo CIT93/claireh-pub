acb10c724db1fed401a9aac197b4926e8fcae52e
ClaireHalverson <cjahalverson@gmail.com>
2022-08-26 04:23:40 +0000

    W3 Learn Together

--- w3/app.js
+++ w3/app.js
@@ -4,15 +4,11 @@ const showOnPage = function (text) {
     let outputDiv = document.getElementById("output")
     outputDiv.append(newParagraph)
   }
+  showOnPage('This output is based on the question which class am I working on, and how much time do I need to invest')
 
-showOnPage('This output is based on the question which class am I working on, and how much time do I need to invest')
-showOnPage('Class today is CIT:' + classnum)
-showOnPage('Homework time is:' + homeworktime)
-showOnPage('Free time is:'+ freetime)
-showOnPage('if freetime is negative. Allocate additonal hours from Friday')
-
- let homework = function (classnum, homeworktime) {
+let homework = function (classnum, homeworktime) {
 let freetime = (4 - homeworktime)
+
 if (classnum = 58) {
 
 } else if (classnum = 28){
@@ -22,8 +18,23 @@ if (classnum = 58) {
 
 } 
 
-return `You have to do work for CIT ${classnum} and you have ${homeworktime} hour(s) of homework. This will leave you with ${freetime} hour(s) of freetime.`
- }
- 
-let homeworkcalc = homework (28,5)
+return `<b> You have to do work for CIT ${classnum} and you have ${homeworktime} hour(s) of homework. This will leave you with ${freetime} hour(s) of freetime.`
+
+}
+
+let homeworkcalc = homework(58,3)
 showOnPage(homeworkcalc)
+
+showOnPage('If freetime is negative. Allocate additonal hours from Friday')
+
+//Function2
+
+let currentday = 3
+let cit93workdue = true
+let cit28workdue = true
+let cit58workdue = false
+
+if (currentday = 2) {
+  return 'Today is tuesday. You have CIT58 tonight at 6pm. No homework'
+  
+}
