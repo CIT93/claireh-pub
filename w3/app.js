8d625ef6acb6a55551bb692d0a5f46894a53c02c
ClaireHalverson <cjahalverson@gmail.com>
2022-08-26 05:27:02 +0000

    Learn Together W3

--- w3/app.js
+++ w3/app.js
@@ -4,37 +4,32 @@ const showOnPage = function (text) {
     let outputDiv = document.getElementById("output")
     outputDiv.append(newParagraph)
   }
+ 
   showOnPage('This output is based on the question which class am I working on, and how much time do I need to invest')
 
 let homework = function (classnum, homeworktime) {
 let freetime = (4 - homeworktime)
 
-if (classnum = 58) {
-
-} else if (classnum = 28){
-
-}else if (classnum = 93){
-
+if (classnum) {
 
 } 
-
 return `<b> You have to do work for CIT ${classnum} and you have ${homeworktime} hour(s) of homework. This will leave you with ${freetime} hour(s) of freetime.`
 
-}
 
-let homeworkcalc = homework(58,3)
+}
+let homeworkcalc = homework (58,2)
 showOnPage(homeworkcalc)
 
 showOnPage('If freetime is negative. Allocate additonal hours from Friday')
 
-//Function2
-
-let currentday = 3
-let cit93workdue = true
-let cit28workdue = true
-let cit58workdue = false
-
-if (currentday = 2) {
-  return 'Today is tuesday. You have CIT58 tonight at 6pm. No homework'
-  
+let classday = function (classT) {
+  if (classT === 2){
+  showOnPage('<b>Today is Tuesday. You have class at 6pm. Leave your house by 5:15pm')
+  } else {
+ showOnPage('<b>No in person class. Refer to homework assignment above.')
+  }
 }
+
+let dayofweek = classday(3)
+
+
